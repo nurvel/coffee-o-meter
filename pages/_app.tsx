fbc495b2b9e16328d0bbd177da0ed5718ef9c6af
Veli-Pekka Nurmi <ext-veli-pekka.nurmi@elisa.fi>
2022-02-11 21:23:43 +0200

    Added mantine UI basic configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,35 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+// import "../styles/globals.css";
 import Layout from "../components/layout";
 
-function MyApp({ Component, pageProps }: AppProps) {
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <>
+        <Head>
+          <title>Page title</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+        </Head>
+
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: "light",
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </>
     </Layout>
   );
 }
-
-export default MyApp;
