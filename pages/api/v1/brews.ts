57431b219fc278d82a2eecc777921818f4c21763
Veli-Pekka Nurmi <ext-veli-pekka.nurmi@elisa.fi>
2022-02-13 11:53:17 +0200

    added mockData and brews respose

---
+++ pages/api/v1/brews.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Brew } from "../../../common/api/generated/models/Brew";
+import { mockData } from "../../../common/mockData";
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Brew | Brew[]>
+) {
+  switch (req.method) {
+    case "GET": {
+      res.status(200).json(mockData.brews);
+      break;
+    }
+    case "POST": {
+      res.status(200).json(mockData.brew);
+      break;
+    }
+    default: {
+      res.status(501);
+      break;
+    }
+  }
+}
