a48db9db404604d401a135fe9d991f5c22d190c2
Veli-Pekka Nurmi <ext-veli-pekka.nurmi@elisa.fi>
2022-02-13 17:46:33 +0200

    implemeted prismaClient as ORM

--- pages/api/v1/brews.ts
+++ pages/api/v1/brews.ts
@@ -1,19 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { ApiError } from "next/dist/server/api-utils";
 import { Brew } from "../../../common/api/generated/models/Brew";
-import { mockData } from "../../../common/mockData";
 
-export default function handler(
+const prisma = new PrismaClient();
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Brew | Brew[]>
+  res: NextApiResponse<Brew | Brew[] | ApiError>
 ) {
   switch (req.method) {
     case "GET": {
-      res.status(200).json(mockData.brews);
+      try {
+        const brews: Brew[] = await prisma.brew.findMany();
+        res.status(200).json(brews);
+        console.log("returned brews");
+      } catch (e) {
+        console.log(e);
+        res.status(404).json(new ApiError(404, JSON.stringify(e)));
+      }
       break;
     }
     case "POST": {
-      res.status(201).json(mockData.brew);
+      const createdBrew: Brew = await prisma.brew.create({
+        data: {
+          dateTime: new Date(),
+        },
+      });
+      res.status(201).json(createdBrew);
       break;
     }
     default: {
