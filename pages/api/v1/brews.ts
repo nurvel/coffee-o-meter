a75b8559baafda98e2c9aaee2351f93a1213a9fb
Veli-Pekka Nurmi <33843131+nurvel@users.noreply.github.com>
2022-03-05 21:12:56 +0200

    Slack notification (#17)
    * post to slack channel when making a brew
    * formatted slack message
    * updated brew threshold input to seconds from minutes
    * refactoring

--- pages/api/v1/brews.ts
+++ pages/api/v1/brews.ts
@@ -1,12 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ApiError } from "next/dist/server/api-utils";
 import { Brew } from "../../../common/api/generated/models/Brew";
+import { createBrew, getAllBrews, getLatestBrew } from "./_brewService";
+import { postToSlackChannel } from "./_slackService";
+import { isThrottleBrew } from "../../../common/utils";
+import { getRandomFact } from "./_factService";
 
-const prisma = new PrismaClient();
-
-const BREW_THRESHOLD_MINUTES = 10;
+const DEFAULT_BREW_THRESHOLD_MINUTES = 10;
+const BREW_THRESHOLD_SECONDS =
+  process.env.NODE_ENV === "development"
+    ? 0
+    : DEFAULT_BREW_THRESHOLD_MINUTES * 60;
 
 export default async function handler(
   req: NextApiRequest,
@@ -15,30 +20,29 @@ export default async function handler(
   switch (req.method) {
     case "GET": {
       try {
-        const brews: Brew[] = await prisma.brew.findMany();
+        const brews: Brew[] = await getAllBrews();
         return res.status(200).json(brews);
       } catch (e) {
         console.log(e);
-        res.status(404).json(new ApiError(404, JSON.stringify(e)));
+        return res.status(404).json(new ApiError(404, JSON.stringify(e)));
       }
-      break;
     }
     case "POST": {
       try {
-        const throttleBrew = await isThrottleBrew(BREW_THRESHOLD_MINUTES);
-        if (throttleBrew)
+        const latestBrew = await getLatestBrew();
+        if (isThrottleBrew(BREW_THRESHOLD_SECONDS, latestBrew)) {
           return res
             .status(425)
             .json(new ApiError(425, "Too early for a new brew!"));
+        }
 
-        const fact = await fetchRandomFact();
-        const createdBrew: Brew = await prisma.brew.create({
-          data: {
-            dateTime: new Date(),
-            fact,
-          },
-        });
-        return res.status(201).json(createdBrew);
+        return getRandomFact()
+          .then((fact) => createBrew(fact))
+          .then((createdBrew) =>
+            postToSlackChannel(createdBrew.fact).then(() =>
+              res.status(201).json(createdBrew)
+            )
+          );
       } catch (e) {
         console.log(e);
         res.status(404).json(new ApiError(404, JSON.stringify(e)));
@@ -51,34 +55,3 @@ export default async function handler(
     }
   }
 }
-
-const fetchRandomFact = async (): Promise<string> => {
-  const RANDOM_FACT_URL = "https://useless-facts.sameerkumar.website/api";
-  return fetch(RANDOM_FACT_URL)
-    .then((res) => res.json())
-    .then((json) => json.data)
-    .catch((err) => {
-      console.log("API error fetching random fact", err);
-      return "I ran out of facts. This time you could discuss the weather";
-    });
-};
-
-const findLatestBrew = async () => {
-  return await prisma.brew.findFirst({
-    orderBy: {
-      dateTime: "desc",
-    },
-    take: 1,
-  });
-};
-
-const isThrottleBrew = async (minutes: number): Promise<boolean> => {
-  const currentDateTime = new Date();
-  const latestBrewDateTime = (await findLatestBrew()).dateTime;
-
-  const throttleSeconds = minutes * 60;
-  return (
-    (currentDateTime.getTime() - latestBrewDateTime.getTime()) / 1000 <
-    throttleSeconds
-  );
-};
