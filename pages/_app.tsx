6af084ea292d11c78da48b918e808f4f82b73986
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2022-03-06 15:06:17 +0200

    added react-query providers to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 //import Layout from "../components/layout";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import React from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { MantineProvider } from "@mantine/core";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
+  const queryClient = React.useRef(new QueryClient());
 
   return (
     //    <Layout>
@@ -16,17 +19,22 @@ export default function App(props: AppProps) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "light",
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
+      <QueryClientProvider client={queryClient.current}>
+        <Hydrate state={pageProps.dehydratedState}>
+          {" "}
+          {/* Place in cache in all components */}
+          <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={{
+              /** Put your mantine theme override here */
+              colorScheme: "light",
+            }}
+          >
+            <Component {...pageProps} />
+          </MantineProvider>
+        </Hydrate>
+      </QueryClientProvider>
     </>
     //    </Layout>
   );
