1a22697c239cae162993718b5629af8aed90c748
Veli-Pekka Nurmi <33843131+nurvel@users.noreply.github.com>
2022-03-06 12:15:06 +0200

    Feature/UI improvements (#19)
    * added images
    * updated favicon
    * commented unused styles out
    * index page layout update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,84 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Button, Paper, Text, Container, Title } from "@mantine/core";
-import { createBrew, getBrews } from "../common/api/uiApiUtis";
+import Image from "next/image";
+import { Button, Text, Container, Title, createStyles } from "@mantine/core";
+import { createBrew } from "../common/api/uiApiUtis";
+import coffeeMascotImg from "../public/coffee-mascot.jpeg";
+
+const useStyles = createStyles((theme, _params) => {
+  return {
+    app: {
+      textAlign: "center",
+    },
+    mascotImage: {
+      border: 0,
+      padding: "30px",
+      maxWidth: "340px",
+      height: "auto",
+      margin: "auto",
+      marginTop: "50px"
+    },
+    title: {
+      margin: "5px",
+    },
+    ingress: {
+      margin: "10px",
+    },
+    button: {
+      margin: "40px",
+    },
+  };
+});
 
 const Home: NextPage = () => {
-  const handeClick = () => {
+  const myStyles = useStyles().classes;
+
+  const handleClick = () => {
     console.log("Brew initiated!");
     createBrew();
   };
 
   return (
-    <Container>
+    <Container size="xs" className={myStyles.app}>
       <Head>
         <title>Coffee-o-meter</title>
         <meta
           name="description"
-          content="See if there is any coffee and alert if you make any"
+          content="Let your colleagues know you made coffee"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Paper padding="md" shadow="xs">
-          <Container>
-            <Title>Coffee-o-meter</Title>
-            <Text>Let your colleges know in Slack </Text>
-          </Container>
-          <Button
-            className="my-button"
-            style={{ backgroundColor: "#000" }}
-            mx={20}
-            size="xl"
-            onClick={() => {
-              handeClick();
-            }}
-          >
-            I made coffee
-          </Button>
+      <div className={myStyles.mascotImage}>
+        <Image alt="Coffee mascot" src={coffeeMascotImg} />
+      </div>
+      <Title className={myStyles.title}>Just made coffee?</Title>
+      <Text className={myStyles.ingress}>
+        Brilliant! Let your colleagues know about it in Slack channel{" "}
+        <b>#coffee-o-meter</b>. Just press the button below.
+      </Text>
+      <Button
+        className={myStyles.button}
+        mx={20}
+        size="xl"
+        color="orange"
+        radius="md"
+        onClick={() => {
+          handleClick();
+        }}
+        // loading={true}
+      >
+        I made coffee
+      </Button>
 
-          <Button
-            className="my-button"
-            style={{ backgroundColor: "#000" }}
-            mx={20}
-            size="xl"
-            onClick={getBrews}
-          >
-            Get brews
-          </Button>
-        </Paper>
-      </main>
+      {/* <Button
+        className="my-button"
+        style={{ backgroundColor: "#000" }}
+        mx={20}
+        size="xl"
+        onClick={getBrews}
+      >
+        Get brews
+      </Button> */}
     </Container>
   );
 };
