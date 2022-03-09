5f247cefa861752ed26c0342b2c1bd68dcf1aa22
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2022-03-09 21:37:27 +0200

    Extracted react-query hooks to own file

---
+++ components/hooks/brewHooks.ts
@@ -0,0 +1,20 @@
+import { createBrew, getBrews } from "../../common/api/uiApiUtils";
+import { useQuery, useMutation } from "react-query";
+import { Brew } from "../../common/api/generated";
+
+import { queryClient } from "../../pages/_app";
+
+export const useCreateBrew = () => {
+  // const queryClient = new QueryClient();
+  //const { mutate, isLoading, isSuccess, isError, error }
+  return useMutation((data) => createBrew(), {
+    onSuccess: () => {
+      console.log(queryClient);
+      queryClient.invalidateQueries("brews");
+    },
+  });
+};
+
+export const useGetBrews = () => {
+  return useQuery<Brew[]>("brews", getBrews);
+};
