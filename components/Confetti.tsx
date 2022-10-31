e9742f5e2cf238ca5afabb2930f6ec0645e3374d
Veli-Pekka Nurmi <33843131+nurvel@users.noreply.github.com>
2022-10-31 08:14:09 +0200

    added viewport height to confetti effect (#37)

--- components/Confetti.tsx
+++ components/Confetti.tsx
@@ -1,15 +1,21 @@
 import React, { FunctionComponent } from "react";
 import { Container } from "@mantine/core";
 import ConfettiExplosion from "react-confetti-explosion";
+import { useViewportSize } from "./hooks/useviewportSize";
 
 interface Props {
   isExplode: boolean;
 }
 
 const Confetti: FunctionComponent<Props> = ({ isExplode }: Props) => {
-  console.log("render: ConfettiExplosion", isExplode);
+  //console.log("render: ConfettiExplosion", isExplode);
+  const { height } = useViewportSize();
 
-  return <Container>{isExplode && <ConfettiExplosion />}</Container>;
+  return (
+    <Container>
+      {isExplode && <ConfettiExplosion floorHeight={height} />}
+    </Container>
+  );
 };
 
 export default React.memo(Confetti);
---
+++ components/hooks/useviewportSize.ts
@@ -0,0 +1,5 @@
+import { useWindowSize } from "usehooks-ts";
+
+export const useViewportSize = () => {
+  return useWindowSize();
+};
