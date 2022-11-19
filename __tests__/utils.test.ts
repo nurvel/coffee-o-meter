f3e97b6d6452419830045097e107cb8375a8d41f
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2022-11-19 19:00:59 +0200

    changes to tests

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -6,22 +6,34 @@ const brew: PrismaBrew = {
   dateTime: new Date(2022, 11, 19, 13, 30, 30),
   fact: "This is a fact",
 };
-const dateNow: number = brew.dateTime.getTime() + 1000;
+const dateNow: number = brew.dateTime.getTime();
 const throttleMilliseconds = 1000;
 
 describe("Get throttle brew boolean", () => {
   test("Throttle is active within one second", () => {
-    expect(isThrottleBrew(throttleMilliseconds, brew, dateNow)).toBe(false);
+    expect(isThrottleBrew(throttleMilliseconds, brew, dateNow + 1000)).toBe(
+      false
+    );
   });
 
   test("Throttle is not  active after one second", () => {
-    expect(isThrottleBrew(throttleMilliseconds + 1, brew, dateNow)).toBe(true);
+    expect(isThrottleBrew(throttleMilliseconds + 1, brew, dateNow + 1000)).toBe(
+      true
+    );
   });
 });
 
 describe("Get throttle milliseconds", () => {
   test("Last brew started 1 sec ago, with 10 sec throttle", () => {
-    expect(getThrottle(dateNow, throttleMilliseconds + 9000, brew)).toBe(9000);
+    expect(getThrottle(dateNow + 1000, throttleMilliseconds + 9000, brew)).toBe(
+      9000
+    );
+  });
+
+  test("Last brew started 11 sec ago, with 10 sec throttle", () => {
+    expect(
+      getThrottle(dateNow + 11000, throttleMilliseconds + 9000, brew)
+    ).toBe(0);
   });
 
   test("No last brew, with 10 sec throttle", () => {
