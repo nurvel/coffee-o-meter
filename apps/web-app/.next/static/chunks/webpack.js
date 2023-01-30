9f27161fed4e67f681a51b21806d92ea967a51bc
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2023-01-30 10:28:06 +0200

    dev command working with db init

--- apps/web-app/.next/static/chunks/webpack.js
+++ apps/web-app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3cfaa3b9081ebf55"; }
+/******/ 		__webpack_require__.h = function() { return "df1d4e04b400c6d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- apps/web-app/.next/static/webpack/webpack.3c0636e34cbeb671.hot-update.js
+++ apps/web-app/.next/static/webpack/webpack.3c0636e34cbeb671.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ad136c5b66748031"; }
+/******/ 	__webpack_require__.h = function() { return "df1d4e04b400c6d9"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
